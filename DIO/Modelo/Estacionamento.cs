b0e8806a446fdd0c0e33a4153dafc94dd29161ce
Bernardo Lucas de Sousa <bernardosaopaulino@gmail.com>
2023-12-14 23:56:12 -0300

    Atividade concluída

---
+++ DIO/Modelo/Estacionamento.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.Modelo
+{
+    class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            string placa = "";
+            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            placa = Console.ReadLine();
+            veiculos.Add(placa);
+        }
+        public void RemoverVeiculo()
+        {
+            string placa = "";
+
+            Console.WriteLine("Digite a placa do veículo para remover:");
+            placa = Console.ReadLine();
+            
+
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+
+                int horas = 0;
+                decimal valorTotal = 0;
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                horas = int.Parse(Console.ReadLine());
+
+                valorTotal = precoInicial + precoPorHora * horas;
+
+                veiculos.Remove(placa);
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+
+        }
+        public void ListarVeiculos()
+        {
+           
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são:");
+                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
+                for(int i = 0; i < veiculos.Count; i++)
+                {
+                    Console.WriteLine("Veiculo com placa: " + veiculos[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+
+
+
+    }
+}
